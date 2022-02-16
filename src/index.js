508a0dff3eebe571a62c3c787650117bd3f8fbf6
Fábio Mantelli <fabiomantelli@Fabios-MacBook-Air.local>
2022-02-15 22:29:46 -0300

    first version of graphql

---
+++ src/index.js
@@ -0,0 +1,69 @@
+const { ApolloServer, gql } = require('apollo-server');
+
+// Toda request é POST
+// Toda request bate no MESMO endpoint (/graphql)
+
+// Query -> Obter informações (GET)
+// Mutation -> Manipular dados (POST/PUT/PATCH/DELETE)
+// Scalar Types -> String, Int, Boolean, Float e ID
+
+
+const typeDefs = gql`
+    type User {
+        _id: ID!
+        name: String!
+        email: String!
+        active: Boolean!
+    }
+
+    type Post {
+        _id: ID!
+        title: String!
+        content: String!
+        author: User!
+    }
+
+    type Query {
+        hello: String
+        users: [User]!
+        getUserByEmail(email: String!): User!
+    }
+
+    type Mutation {
+        createUser(name: String!, email: String!): User!
+    }
+`;
+
+const users = [
+    { _id: String(Math.random()), name: 'Fabio', email: 'fmmantelli@gmail.com', active: true },
+    { _id: String(Math.random()), name: 'Joao', email: 'joao@gmail.com', active: true },
+    { _id: String(Math.random()), name: 'Paulo', email: 'pasehn@gmail.com', active: false },
+    { _id: String(Math.random()), name: 'Pablo', email: 'pablomarcal@gmail.com', active: true },
+]
+
+const resolvers = {
+    Query: {
+        hello: () => 'Hello Fabio',
+        users: () => users,
+        getUserByEmail: (_, args) => {
+            return users.find(user => user.email === args.email);
+        }
+
+    },
+    Mutation: {
+        createUser: (_, args) => {
+            const newUser = {
+                _id: String(Math.random()),
+                name: args.name,
+                email: args.email,
+                active: true
+            }
+            users.push(newUser);
+            return newUser;
+        }
+    }
+};
+
+const server = new ApolloServer({ typeDefs, resolvers });
+
+server.listen().then(({ url }) => console.log(`Server started at ${url}`));
